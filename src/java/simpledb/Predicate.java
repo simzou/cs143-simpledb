dec90182b52ae8c18b2ce023b6929fd2cd8a0bf1
Simon Zou <simon.zou@gmail.com>
2014-04-29 16:56:28 -0700

    implemented predicate

--- src/java/simpledb/Predicate.java
+++ src/java/simpledb/Predicate.java
@@ -9,6 +9,9 @@ public class Predicate implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    private int m_field;
+    private Op m_op;
+    private Field m_operand;
     /** Constants used for return codes in Field.compare */
     public enum Op implements Serializable {
         EQUALS, GREATER_THAN, LESS_THAN, LESS_THAN_OR_EQ, GREATER_THAN_OR_EQ, LIKE, NOT_EQUALS;
@@ -55,6 +58,9 @@ public class Predicate implements Serializable {
      *            field value to compare passed in tuples to
      */
     public Predicate(int field, Op op, Field operand) {
+    	m_field = field;
+    	m_op = op;
+    	m_operand = operand;
         // some code goes here
     }
 
@@ -64,7 +70,7 @@ public class Predicate implements Serializable {
     public int getField()
     {
         // some code goes here
-        return -1;
+        return m_field;
     }
 
     /**
@@ -73,7 +79,7 @@ public class Predicate implements Serializable {
     public Op getOp()
     {
         // some code goes here
-        return null;
+        return m_op;
     }
     
     /**
@@ -82,7 +88,7 @@ public class Predicate implements Serializable {
     public Field getOperand()
     {
         // some code goes here
-        return null;
+        return m_operand;
     }
     
     /**
@@ -97,7 +103,8 @@ public class Predicate implements Serializable {
      */
     public boolean filter(Tuple t) {
         // some code goes here
-        return false;
+    	Field tuple_field = t.getField(getField());
+    	return tuple_field.compare(getOp(), m_operand);
     }
 
     /**
@@ -106,6 +113,6 @@ public class Predicate implements Serializable {
      */
     public String toString() {
         // some code goes here
-        return "";
+        return "f = " + m_field + " op = " + m_op.toString() + " operand = " + m_operand;
     }
 }
